7e68c051df61481d8f1a5d53c2cef48cc1e8262f
DashaZhebko <daryna.zhebko@gmail.com>
2023-08-15 21:57:52 +0300

    поправила верхню границю

--- Src/HomeWork11.java
+++ Src/HomeWork11.java
@@ -3,11 +3,11 @@ import java.util.Random;
 
 public class HomeWork11 {
     public static int MIN_BOUND = 11;
-    public static int MAX_BOUND = 100000;
+    public static int MAX_BOUND = 100011;
     public static void main(String[] args) {
         Random random = new Random();
 
-        int num = random.nextInt(MAX_BOUND - MIN_BOUND + 1) + MIN_BOUND;
+        int num = random.nextInt(MAX_BOUND - MIN_BOUND +1) + MIN_BOUND;
         System.out.println("Число: " + num);
         int sum = 0;
         while (num>0){
